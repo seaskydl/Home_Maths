d89572eef831fe293181f7692173091e6e59f066
Jennifer <jenniferli0208@outlook.com>
2025-04-28 18:36:44 +1000

    init

---
+++ main.py
@@ -0,0 +1,23 @@
+def welcome():
+  print("Welcome to Home Maths Helper")
+
+def readPic():
+  print("Step 1: Read Pic")
+
+def identifyPic():
+  print("Step 2: Identify Pic")
+
+def verify():
+  print("Step 3: Verify")
+
+def doHomework():
+  readPic()
+  identifyPic()
+  verify()
+
+def main():
+  welcome()
+  doHomework()
+
+if __name__ == "__main__":
+  main()
